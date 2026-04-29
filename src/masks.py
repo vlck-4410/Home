afe85d743daece9fc263bf397c88f9c3c0b7ad8d
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-04-29 19:28:19 +1000

    add_gitignore

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def mask_account(account: str) -> str:
+    """Маскировка номера акканта и вывод последних 4 цифр"""
+    mask_account = "**" + account[-4:]
+    return mask_account
+
+def get_mask_card_number(card_number: str) -> str:
+    """Маскировка номера карты и вывод первых 6 и последних 4 цифр с пробелами после каждой 4-ой цифры"""
+    mask_card = card_number[:6] + "*" * 6 + card_number[-4:]
+    result = " ".join(mask_card[i : i + 4] for i in range(0, len(mask_card), 4))
+
+    return result
+
+
