5cbe607fd980b35f2d8104f477d60d9183e67c7b
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-04-29 20:20:19 +1000

    add_get_date

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,26 @@
 from masks import get_mask_card_number, mask_account
 
-def mask_account_card(account_card:str) -> str:
+
+def mask_account_card(account_card: str) -> str:
     account_card = account_card.title()
-    account = 'Счет'
+    account = "Счет"
     letters = []
     if account in account_card:
-        account_card = account + ' ' + mask_account(account_card)
+        account_card = account + " " + mask_account(account_card)
         result = account_card
     else:
         for letter in account_card:
             if letter.isalpha():
-              letters.append(letter)
-        letters = ''.join(letters)
-        account_card = account_card.replace(letters, '')
+                letters.append(letter)
+        letters = "".join(letters)
+        account_card = account_card.replace(letters, "")
         result = letters + get_mask_card_number(account_card)
 
     return result
 
 
+def get_date(date: str) -> str:
+    parts = date[:-16].split("-")
+    fixed_date = f"{parts[2]}.{parts[1]}.{parts[0]}"
+
+    return fixed_date
