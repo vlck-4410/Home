da44e568e07098ec580409de77d94a24825b9cfa
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-05-05 23:37:44 +1000

    some more fixes

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_dict: list, state = "EXECUTED") -> list:
+def filter_by_state(list_dict: list, state="EXECUTED") -> list:
     """Принимает список словарей и значение для ключа state, возвращает новый список,
     со словарями, в которых state соответсвтует указанному значению."""
     sorted_dict = []
@@ -6,15 +6,12 @@ def filter_by_state(list_dict: list, state = "EXECUTED") -> list:
         if one_dict["state"] == state:
             sorted_dict.append(one_dict)
 
-
     return sorted_dict
 
 
-def sort_by_date(list_dict: list, is_reverse=True) -> list:
+def sort_by_date(list_dict: list, reverse: bool = True) -> list:
     """Принимает список словарей и необязательный параметр сортировки по дате,
     если параметр не указан, сортирует по убыванию"""
-    sorted_dict_date = sorted(list_dict, key=lambda k: k["date"], reverse = is_reverse)
+    sorted_dict_date = sorted(list_dict, key=lambda k: k["date"], reverse=reverse)
 
     return sorted_dict_date
-
-
