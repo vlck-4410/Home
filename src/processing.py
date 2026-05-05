38be63cc5715693425bfa39c12369c17ee9f6fa2
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-05-05 22:12:45 +1000

    add sort_by_date function

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,20 @@
-def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+def filter_by_state(list_dict: list, state = "EXECUTED") -> list:
     """Принимает список словарей и значение для ключа state, возвращает новый список,
     со словарями, в которых state соответсвтует указанному значению"""
-    filtered_datas = []
+    sorted_dict = []
     for one_dict in list_dict:
         if one_dict["state"] == state:
-            filtered_datas.append(one_dict)
-        elif state != "EXECUTED" and state != "CANCELLED":
-            return "Неверно введено занчение 'state'"
-
-    return filtered_datas
+            sorted_dict.append(one_dict)
 
 
-def sort_by_date(list_dict: list, reverse=True) -> list:
+    return sorted_dict
+
+
+def sort_by_date(list_dict: list, is_reverse=True) -> list:
     """Принимает список словарей и необязательный параметр сортировки по дате,
     если параметр не указан, сортирует по убыванию"""
-    sorted_datas = sorted(list_dict, key=lambda k: k["date"], reverse=reverse)
+    sorted_dict_date = sorted(list_dict, key=lambda k: k["date"], reverse = is_reverse)
 
-    return sorted_datas
+    return sorted_dict_date
+
+
