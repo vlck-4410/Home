71c8d811964bccf7ccc074a2cce37cf862c89106
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-05-20 14:23:33 +1000

    add card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -14,5 +14,13 @@ def transaction_descriptions(list_dict):
             yield description.get('description', '')
         except (TypeError):
             continue
+def card_number_generator(start_number, end_number):
+    return (f"{num_str[:4]} {num_str[4:8]} {num_str[8:12]} {num_str[12:16]}" for num_str in (f"{n:016d}" for n in range(start_number, end_number + 1)))
+
+
+
+
+for card_number in card_number_generator(1, 100000):
+    print (card_number)
 
 
