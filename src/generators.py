fe6a2d65333dacbfd517a1c2d4875f342cde088b
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-05-23 13:20:05 +1000

    add tests and html cov

--- src/generators.py
+++ src/generators.py
@@ -7,12 +7,19 @@ def filter_by_currency(list_dict, x=None):
         yield 'Нет данных'
         return
 
+    found_any = False
+
     for transaction in list_dict:
         try:
-            if transaction.get("operationAmount", {}).get("currency", {}).get("code", {}) == x:
+            currency_code = transaction.get("operationAmount", {}).get("currency", {}).get("code", {})
+            if currency_code == x:
                 yield transaction
-        except TypeError:
+                found_any = True
+        except (TypeError, AttributeError):
             continue
+    if not found_any:
+        yield 'Нет данных'
+
 
 
 def transaction_descriptions(list_dict):
@@ -32,87 +39,14 @@ def transaction_descriptions(list_dict):
 
 
 def card_number_generator(start_number, end_number):
-    return (
-        f"{num_str[:4]} {num_str[4:8]} {num_str[8:12]} {num_str[12:16]}"
-        for num_str in (f"{n:016d}" for n in range(start_number, end_number + 1))
-    )
+    if start_number == 0 or end_number == 0:
+        yield 'Введите значения'
+        return
+
+    for n in range(start_number, end_number + 1):
+        card_number = f"{n:016d}"
+        yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}"
 
 
-description = filter_by_currency([{
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
 
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {
-                "amount": "67314.70",
-                "currency": {
-                    "name": "руб.",
-
-                }
-            },
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657"
-        }], x="RUB")
-for x in range(1):
-    print(*description)
 
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -89,12 +89,14 @@ def test_generator_transaction_descriptions():
    for expected in expected_results:
        assert next(generator) == expected
 
+
 def test_generator_transaction_descriptions_zero_data():
     test_data = ([])
     generator = transaction_descriptions(test_data)
     expected_results = ['Данные отсутствуют']
     for expected in expected_results:
         assert next(generator) == expected
+
 
 def test_generator_transaction_descriptions_less_key_description():
     test_data = ([
@@ -187,15 +189,17 @@ def test_filter_by_currency_zero_data():
     for expected in expected_results:
         assert next(generator) == expected
 
+
 def test_filter_by_currency_zero_data_with_currency():
-    test_data = ([], x=='RUB')
+    test_data = ([])
     generator = filter_by_currency(test_data)
     expected_results = ['Нет данных']
     for expected in expected_results:
         assert next(generator) == expected
 
+
 def test_filter_by_currency_less_key_data():
-    test_data = ([{
+    test_data = [{
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
@@ -269,11 +273,26 @@ def test_filter_by_currency_less_key_data():
             "description": "Перевод организации",
             "from": "Visa Platinum 1246377376343588",
             "to": "Счет 14211924144426031657"
-        }], x == 'RUB')
-    generator = filter_by_currency(test_data)
+        }]
+
+    generator = filter_by_currency(test_data, "RUB")
     expected_results = ['Нет данных']
     for expected in expected_results:
         assert next(generator) == expected
+
+
+@pytest.mark.parametrize('start_number, end_number, expected', [
+    (0, 0,['Введите значения']),
+    (1, 5, ['0000 0000 0000 0001',
+                        '0000 0000 0000 0002',
+                        '0000 0000 0000 0003',
+                        '0000 0000 0000 0004',
+                        '0000 0000 0000 0005']),
+     (1, 1, ['0000 0000 0000 0001'])
+ ])
+def test_card_number_generator(start_number, end_number, expected):
+    assert list(card_number_generator(start_number, end_number)) == expected
+
 
 
 
