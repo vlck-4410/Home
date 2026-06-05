8fadb3eecee8c3360e96cc17f60cd2263bb8df6a
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-06-05 19:28:12 +1000

    add tests for funcs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,10 +8,23 @@ def test_transactions_list_success_with_patch():
                             "operationAmount":{"amount":"31957.58", "currency":{"name": "руб.", "code": "RUB"}}}]
     json_string = json.dumps(mock_transcriptions)
 
-    with patch('src.utils.os.patch.exists', return_value=True):
+    with patch('src.utils.os.path.exists', return_value=True):
         with patch('builtins.open', mock_open(read_data=json_string)):
             result = transactions_list('fake_path/operations.json')
     assert result == mock_transcriptions
+
+
+def test_transactions_list_file_not_found():
+    with patch('src.utils.os.path.exists', return_value=False):
+        result = transactions_list('fake_path/operations.json')
+        assert result == []
+
+def test_transactions_list_invalid_json_with_patch():
+    broken_json_string = "я не json"
+    with patch('src.utils.os.path.exists', return_value=True):
+        with patch('builtins.open', mock_open(read_data=broken_json_string)):
+            result = transactions_list('fake_path/operations.json')
+            assert result == []
 
 
 
