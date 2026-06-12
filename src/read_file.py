f814c218da7ff7ea3d4ba5c082bf2b8d26e35def
Zarubin Sergey <seregazarubin2003@mail.ru>
2026-06-12 16:45:10 +1000

    add `read_file_csv` and `read_file_excel` funcs, add some descriptions in README, add some tests for funcs

--- src/read_file.py
+++ src/read_file.py
@@ -1,9 +1,10 @@
 import csv
-
 import pandas as pd
 
 
 def read_file_csv(file_dir) -> list:
+    """Функция принимает на вход путь к файлу csv, на выход выдает
+    список словарей с транзакциями из этого файла"""
     try:
         with open(file_dir, encoding="utf8") as csvfile:
             transactions_from_csv = list(csv.DictReader(csvfile))
@@ -14,6 +15,8 @@ def read_file_csv(file_dir) -> list:
 
 
 def read_file_excel(file_dir) -> list:
+    """Функция принимает на вход путь к файлу xlsx, на выход выдает
+     список словарей с транзакциями из этого файла"""
     try:
         df = pd.read_excel(file_dir)
         transactions_from_excel = df.to_dict("records")
